d08320f58a927614d7467c0f4ad4d551d71f7ac8
Thiago Granja <thiagoaagranja@outlook.com>
2023-05-02 16:51:56 -0300

    feat: transfer & statement controllers and validation created

--- src/controllers/depositController.js
+++ src/controllers/depositController.js
@@ -3,11 +3,11 @@ const { format } = require('date-fns');
 
 const makeDeposit = (req, res) => {
     const { accountNumber, amount } = req.body;
-    const {accounts, deposits} = database;
-    const date = format(new Date(), "uuuu-MM-dd HH:mm:ss" )
-    const depositInfo = {date, accountNumber, amount};
+    const { accounts, deposits } = database;
+    const date = format( new Date(), "uuuu-MM-dd HH:mm:ss" );
+    const depositInfo = { date, accountNumber, amount };
 
-    const account = accounts.find(account => account.number === accountNumber);
+    const account = accounts.find( account => account.number === accountNumber );
     
     account.balance = (account.balance + +amount);
 
